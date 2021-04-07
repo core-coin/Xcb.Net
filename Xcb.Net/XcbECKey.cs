d2445001ddbf642c414c8301ced359d04ce0d6f7
Mojbaba <mojbaba@wallit.eu>
2021-04-07 04:59:01 +0430

    XcbECKey keys features added

---
+++ Xcb.Net/XcbECKey.cs
@@ -0,0 +1,69 @@
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math.EC.Rfc8032;
+using Org.BouncyCastle.Security;
+using Xcb.Net.Extensions;
+
+namespace Xcb.Net.Signer
+{
+    public class XcbECKey
+    {
+        private static readonly SecureRandom _secureRandom = new SecureRandom();
+        Ed448PrivateKeyParameters _privateKey;
+
+        byte[] _privateKeyBytes = null;
+        byte[] _publicKeyBytes = null;
+
+        string _privateKeyHex = null;
+        string _publicKeyHex = null;
+
+        public XcbECKey(string privateKey) : this(privateKey.HexToByteArray())
+        { }
+
+        public XcbECKey(byte[] privateKey)
+        {
+            _privateKey = new Ed448PrivateKeyParameters(privateKey, 0);
+        }
+
+        public byte[] GetPrivateKeyBytes()
+        {
+            return _privateKeyBytes ?? (_privateKeyBytes = _privateKey.GetEncoded());
+        }
+
+        public byte[] GetPublicKeyBytes()
+        {
+            return _publicKeyBytes ?? (_publicKeyBytes = _privateKey.GeneratePublicKey().GetEncoded());
+        }
+
+        public string GetPrivateKeyHex()
+        {
+            return _privateKeyHex ?? (_privateKeyHex = GetPrivateKeyBytes().ToHex());
+        }
+
+        public string GetPublicKeyHex()
+        {
+            return _publicKeyHex ?? (_publicKeyHex = GetPublicKeyBytes().ToHex());
+        }
+
+        public static XcbECKey GenerateKey(byte[] seed)
+        {
+            var secureRandom = _secureRandom;
+            if (seed != null)
+            {
+                secureRandom = new SecureRandom();
+                secureRandom.SetSeed(seed);
+            }
+
+            var gen = new Ed448KeyPairGenerator();
+            var keyGenParam = new Ed448KeyGenerationParameters(secureRandom);
+            gen.Init(keyGenParam);
+            var keyPair = gen.GenerateKeyPair();
+            var privateBytes = ((Ed448PrivateKeyParameters)keyPair.Private).GetEncoded();
+
+            return new XcbECKey(privateBytes);
+        }
+
+
+
+    }
+}
