cb92a34ba08c2e1a848ee9dc6c5e4fa38eb0b444
Mojbaba <mojbaba@wallit.eu>
2022-04-04 12:26:38 +0430

    fix tests

--- Xcb.Net.Test/TransactionTest.cs
+++ Xcb.Net.Test/TransactionTest.cs
@@ -11,7 +11,7 @@ namespace Xcb.Net.Test
         public void TransactionFullTest()
         {
             //Given
-            XcbECKey key = new XcbECKey("69bb68c3a00a0cd9cbf2cab316476228c758329bbfe0b1759e8634694a9497afea05bcbf24e2aa0627eac4240484bb71de646a9296872a3c8e", 11);
+            XcbECKey key = new XcbECKey("69bb68c3a00a0cd9cbf2cab316476228c758329bbfe0b1759e8634694a9497afea05bcbf24e2aa0627eac4240484bb71de646a9296872a3c0e", 11);
 
             Transaction transaction = new Transaction(to: "ce276773ac97d16855a3c8faa45399136b56d4194860",
             amount: BigInteger.Parse("200"),
@@ -31,6 +31,60 @@ namespace Xcb.Net.Test
             //Then
             Assert.Equal(encoded, reEncode);
             Assert.Equal("0xf8ce800a830f423f8096ce276773ac97d16855a3c8faa45399136b56d419486081c880b8ab448eafc4ad76f52262dc04c09738e017dbf7dac5cee6d7bf0a8c0b60aaa1403d10e3d3a28f2d0ce9a9ffb64ebb9e0a59a3637f0f48aa597f80722d2c29acab15b7e2677f3df91ea86ecbb0f6cc871fdf39a154262ed467ae6e2996cdc09dbce205c318b7581d28bae84c0eb3d118edf61000315484db568379ce94f9c894e3e6e4c7ee216676b713ca892d9b26746ae902a772e217a6a8bb493ce2bb313cf0cb66e76765d4c45ec6b68600",
+                encodedHex);
+        }
+
+        [Fact]
+        public void TransactionWithNormalPrivateKeyTest()
+        {
+            //Given
+            XcbECKey key = new XcbECKey("e90d87a86d14321252ed4d1c7a5652b62112a8269343c04c1a69a5e218710371aa20e54f75b2b9d5fea220b5739e5fa93ec6ea92ee9ec92c00", 1);
+
+            Transaction transaction = new Transaction(to: "cb3599b799bc6db3d54d496558fc2282cb70f72e8c2b",
+            amount: BigInteger.Parse("500"),
+            nonce: BigInteger.Parse("1"),
+            energyLimit: BigInteger.Parse("99999"),
+            energyPrice: BigInteger.Parse("1000000000"),
+            data: "");
+
+            //When
+            transaction.Sign(key, 0);
+            var encoded = transaction.GetRlpEncoding();
+            var encodedHex = encoded.ToHex(true);
+
+            var transaction2 = Transaction.Decode(encoded);
+            var reEncode = transaction2.GetRlpEncoding();
+
+            //Then
+            Assert.Equal(encoded, reEncode);
+            Assert.Equal("0xf8d301843b9aca008301869f8096cb3599b799bc6db3d54d496558fc2282cb70f72e8c2b8201f480b8ab75ba144a0aad7a63d6bd89972c69ce7e4e25d484aa4f8d33fbfc06fa76ffce1364e0054d5225a86bf66c91d2697dcb2f75bdd9dd35e34d1c80dcd67f147902ac0bc08d03e924fb4ee5016b793d2d5e31d0dbe21a2dda17d68060796de8c3fe325dc0c8fb98dcfb672f2a4aeddb245eb51f0067b522b53a55363b334c30eec79b491aef0bd4dbd59c9364b9f2e5890487c635825a681dd316f8ce010ccbbaa12ddfe8fc423a6c8f64e3f380",
+                encodedHex);
+        }
+
+        [Fact]
+        public void TransactionWithExtendedPrivateKeyTest()
+        {
+            //Given
+            XcbECKey key = new XcbECKey("e90d87a86d14321252ed4d1c7a5652b62112a8269343c04c1a69a5e218710371aa20e54f75b2b9d5fea220b5739e5fa93ec6ea92ee9ec92cdd", 11);
+
+            Transaction transaction = new Transaction(to: "cb3599b799bc6db3d54d496558fc2282cb70f72e8c2b",
+            amount: BigInteger.Parse("500"),
+            nonce: BigInteger.Parse("2"),
+            energyLimit: BigInteger.Parse("99999"),
+            energyPrice: BigInteger.Parse("1000000000"),
+            data: "");
+
+            //When
+            transaction.Sign(key, 0);
+            var encoded = transaction.GetRlpEncoding();
+            var encodedHex = encoded.ToHex(true);
+
+            var transaction2 = Transaction.Decode(encoded);
+            var reEncode = transaction2.GetRlpEncoding();
+
+            //Then
+            Assert.Equal(encoded, reEncode);
+            Assert.Equal("0xf8d302843b9aca008301869f8096cb3599b799bc6db3d54d496558fc2282cb70f72e8c2b8201f480b8abffd9a99602b23626778212ca5c679163ab5d8a89b1506d36f2b10971c3d2a76ab296d0348a1fda23ccde3bcca2f209c74a951da7845305c2804e6a556991f0f8b5a7d1843399bdaa807fcf7d0b3c04d29ad9d2f81d809c55cbad50ce0ae80eb7ae93b2999b0bfdd67b5856d6c2cf8a9e10000b7f8b323c7ed6ee7539452cfb0578a990e90a6a7401174c941267c411289c452152d2d7cbe15986d662935f2ea989be074aaf0966251fc700",
                 encodedHex);
         }
 
---
+++ Xcb.Net/HDWallet/HDWallet.cs
@@ -0,0 +1,136 @@
+using System;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Xcb.Net.Signer;
+using Org.BouncyCastle.Math.EC.Rfc8032;
+
+namespace Xcb.Net.HDWallet
+{
+    public class HDWallet
+    {
+        public byte[] pbkdf2_sha3_256(byte[] password, byte[] salt, int iterations, int hashByteSize)
+        {
+            var pdb = new Pkcs5S2ParametersGenerator(new Org.BouncyCastle.Crypto.Digests.Sha3Digest(512));
+            pdb.Init(password, salt,
+                         iterations);
+            var key = (KeyParameter)pdb.GenerateDerivedMacParameters(hashByteSize * 8);
+            return key.GetKey();
+        }
+
+        public byte[] shaHash(byte[] password, byte[] salt) {
+            return pbkdf2_sha3_256(password, salt, 2048, 57);
+        }
+
+        public byte[] concatenateAndHex(byte prefix, byte[] password, uint index, byte[] salt)
+        {
+            byte[] result = new byte[62];
+            result[0] = prefix;
+            Array.Copy(password, 0, result, 1, 57);
+            for (int i = 58; i < 62; i++) {
+                result[i] = (byte) (index & 0xff);
+                index >>= 8;
+            }
+            return shaHash(result, salt);
+        }
+
+        public void reducePrivate(byte[] key) {
+            key[56] = 0;
+            key[55] = 0;
+            key[54] = 0;
+            key[53] = 0;
+            key[0] &= 0xfc;
+        }
+
+        public byte[] addTwoSecrets(byte[] key1, byte[] key2) {
+            byte[] key = new byte[57];
+            uint count = 0;
+            for (int i = 0; i < 57; i++) {
+                count += (uint) (key1[i]) + (uint) (key2[i]);
+                key[i] = (byte) (count & 0xff);
+                count >>= 8;
+            }
+            return key;
+        }
+
+        public byte[] seedToMaster(byte[] seed) {
+            if (seed.Length != 64) {
+                throw new Exception("Length of seed must be 64");
+            }
+
+            System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+
+            byte[] part1 = shaHash(seed, enc.GetBytes("mnemonicforthechain"));
+            byte[] part2 = shaHash(seed, enc.GetBytes("mnemonicforthekey"));
+            byte[] result = new byte[114];
+            Array.Copy(part1, 0, result, 0, 57);
+            Array.Copy(part2, 0, result, 57, 57);
+            result[113] |= 0x80;
+            result[112] |= 0x80;
+            result[112] &= 0xbf;
+            return result;
+        }
+
+        public byte[] extendedPrivateToPublic(byte[] extendedKey) {
+            
+            byte[] privateKey = new byte[57];
+            Array.Copy(extendedKey, 57, privateKey, 0, 57);
+            var key = new XcbECKey(privateKey, 1);
+            var pub = key.GetPublicKeyBytes();
+            byte[] extendedPublic = new byte[114];
+            Array.Copy(extendedKey, 0, extendedPublic, 0, 57);
+            Array.Copy(pub, 0, extendedPublic, 57, 57);
+            return extendedPublic;
+        }
+
+        public byte[] childPrivateToPrivate(byte[] extPrivate, uint index) {
+            
+            byte[] child = new byte[114];
+            byte[] chain = new byte[57];
+            Array.Copy(extPrivate, 0, chain, 0, 57);
+            byte[] priv = new byte[57];
+            Array.Copy(extPrivate, 57, priv, 0, 57);
+            var key = new XcbECKey(priv, 1);
+            var pub = key.GetPublicKeyBytes();
+            
+            if (index >= 0x80000000) {
+                var hex = concatenateAndHex(1, priv, index, chain);
+                Array.Copy(hex, 0, child, 0, 57);
+                hex = concatenateAndHex(0, priv, index, chain);
+                reducePrivate(hex);
+                var a = addTwoSecrets(priv, hex);
+                Array.Copy(a, 0, child, 57, 57);
+            } else {
+                var hex = concatenateAndHex(3, pub, index, chain);
+                Array.Copy(hex, 0, child, 0, 57);
+                hex = concatenateAndHex(2, pub, index, chain);
+                reducePrivate(hex);
+                var a = addTwoSecrets(priv, hex);
+                Array.Copy(a, 0, child, 57, 57);
+            }
+            return child;
+        }
+
+        public byte[] childPublicToPublic(byte[] extPublic, uint index) {
+            
+            byte[] child = new byte[114];
+            byte[] chain = new byte[57];
+            Array.Copy(extPublic, 0, chain, 0, 57);
+            byte[] pub = new byte[57];
+            Array.Copy(extPublic, 57, pub, 0, 57);
+            byte[] result = new byte[57];
+            
+            if (index >= 0x80000000) {
+                throw new Exception("Cant retrieve public key from hardened parent key");
+            } else {
+                var hex = concatenateAndHex(3, pub, index, chain);
+                Array.Copy(hex, 0, child, 0, 57);
+                hex = concatenateAndHex(2, pub, index, chain);
+                reducePrivate(hex);
+                Ed448.ShiftPublic(pub, hex, result);
+                Array.Copy(result, 0, child, 57, 57);
+            }
+            return child;
+        }
+
+    }
+}
