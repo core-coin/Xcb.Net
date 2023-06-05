2cfc9c2d7341e872b10ceab8df6b83818daa98a8
Mojbaba <mojbaba@wallit.eu>
2023-06-05 14:27:02 +0330

    add some Tests

--- Xcb.Net.Test/BIP39Test.cs
+++ Xcb.Net.Test/BIP39Test.cs
@@ -16,9 +16,21 @@ namespace Xcb.Net.Test
 
             // When
             var mnemonic = new BIP39.Mnemoic(validMnemonic);
-        
+
             // Then
             Assert.NotNull(mnemonic);
+        }
+
+        [Fact]
+        public void InvalidMnemonic()
+        {
+            // Given
+            var validMnemonic = "zoo genuine pelican eagle blouse emotion refuse fringe flock salute space climb marriage empower feature inform ostrich endless fault barely chronic shy couple wonder";
+
+
+            // When
+            Assert.Throws<ArgumentException>(() => new BIP39.Mnemoic(validMnemonic));
+
         }
     }
 }
